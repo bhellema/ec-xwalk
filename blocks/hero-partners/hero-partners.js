ed9400528f2f525c8266ab057eb120a4ab62c611
Ben Helleman <bhellema@adobe.com>
2026-02-01 20:47:44 -0500

    partners

---
+++ blocks/hero-partners/hero-partners.js
@@ -0,0 +1,7 @@
+export default function decorate(block) {
+  // Hero Partners block - minimal decoration
+  // Block structure: Row 1 = background image, Row 2 = heading text
+
+  // Ensure block has the hero-partners class
+  block.classList.add('hero-partners');
+}
